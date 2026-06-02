cbf87d699822c419e68252dd1018a1a7d6938de6
AreenVisions <content@areenvisions.com>
2026-06-02 19:15:03 +0300

    Added a separate ar file for locale

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,341 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get iron_vault => 'الخزنة الحديدية';
+
+  @override
+  String get credentials_secured => 'تم تأمين بيانات الاعتماد:';
+
+  @override
+  String get main_view_string =>
+      'تم تشفير بيانات الاعتماد وتخزينها محليًا على جهازك';
+
+  @override
+  String get search_the_vault => 'ابحث في الخزنة';
+
+  @override
+  String get vault => 'الخزنة';
+
+  @override
+  String get pass_generator => 'مولد كلمات المرور';
+
+  @override
+  String get password_generator => 'مولد كلمات المرور';
+
+  @override
+  String get generated_password => 'كلمة المرور المُنشأة';
+
+  @override
+  String get generate_password => 'انقر على إنشاء كلمة مرور جديدة';
+
+  @override
+  String get copy => 'نسخ';
+
+  @override
+  String get generate => 'إنشاء';
+
+  @override
+  String get password_complexity => 'تعقيد كلمة المرور';
+
+  @override
+  String get poor => 'ضعيف';
+
+  @override
+  String get ok => 'جيد';
+
+  @override
+  String get fine => 'جيد';
+
+  @override
+  String get great => 'ممتاز';
+
+  @override
+  String get excellent => 'ممتاز';
+
+  @override
+  String get password_length => 'طول كلمة المرور';
+
+  @override
+  String get alphabet => 'الأبجدية';
+
+  @override
+  String get aabbcc => 'AaBbCc';
+
+  @override
+  String get numbers => 'الأرقام';
+
+  @override
+  String get numbers_string => '0123';
+
+  @override
+  String get symbols => 'الرموز';
+
+  @override
+  String get symbols_string => '!@#\$%';
+
+  @override
+  String get new_entry => 'إدخال جديد';
+
+  @override
+  String get forge_new_entry => 'إنشاء إدخال جديد';
+
+  @override
+  String get new_entry_string => '.تهيئة مجموعة جديدة من بيانات الاعتماد';
+
+  @override
+  String get title => 'اللقب';
+
+  @override
+  String get username => 'اسم المستخدم';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get required => '*مطلوب';
+
+  @override
+  String get password_generation_settings => 'إعدادات إنشاء كلمة المرور';
+
+  @override
+  String get extra_notes => 'ملاحظات إضافية';
+
+  @override
+  String get notes_go_here => 'ضع الملاحظات هنا...';
+
+  @override
+  String get secure_entry => 'إدخال آمن';
+
+  @override
+  String get entry_details => 'تفاصيل الإدخال';
+
+  @override
+  String get current_entry => 'تفاصيل الإدخال';
+
+  @override
+  String get security_health => 'حالة الأمان';
+
+  @override
+  String get date_created => 'تاريخ الإنشاء';
+
+  @override
+  String get settings => 'الإعدادات';
+
+  @override
+  String get security_protocols => 'بروتوكولات الأمان';
+
+  @override
+  String get pin_lock => 'PIN قفل';
+
+  @override
+  String get pin_lock_string =>
+      '.قم بإعداد رقم تعريف شخصي مكون من 6 أرقام كطبقة أمان إضافية';
+
+  @override
+  String get pin_unlock_string =>
+      ' تم تعيين رمز مسبقًا، انقر هنا لتغييره، اضغط مطولًا لإزالته';
+
+  @override
+  String get clipboard_clearing => 'مسح الحافظة';
+
+  @override
+  String get clipboard_clearing_string =>
+      '.يمسح الحافظة بعد 30 ثانية من نسخ بيانات الاعتماد';
+
+  @override
+  String get vault_preferences => 'إعدادات الخزنة';
+
+  @override
+  String get dark_mode => 'الوضع الداكن';
+
+  @override
+  String get dark_mode_string => 'ثيم Ironclad Obsidian';
+
+  @override
+  String get system_language => 'لغة النظام';
+
+  @override
+  String get data_operations => 'عمليات البيانات';
+
+  @override
+  String get export_credentials => 'تصدير بيانات الاعتماد';
+
+  @override
+  String get wipe_the_vault => 'مسح الخزنة';
+}
+
+/// The translations for Arabic, as used in Egypt (`ar_EG`).
+class AppLocalizationsArEg extends AppLocalizationsAr {
+  AppLocalizationsArEg() : super('ar_EG');
+
+  @override
+  String get iron_vault => 'الخزنة الحديدية';
+
+  @override
+  String get credentials_secured => 'تم تأمين بيانات الاعتماد:';
+
+  @override
+  String get main_view_string =>
+      'تم تشفير بيانات الاعتماد وتخزينها محليًا على جهازك';
+
+  @override
+  String get search_the_vault => 'ابحث في الخزنة';
+
+  @override
+  String get vault => 'الخزنة';
+
+  @override
+  String get pass_generator => 'مولد كلمات المرور';
+
+  @override
+  String get password_generator => 'مولد كلمات المرور';
+
+  @override
+  String get generated_password => 'كلمة المرور المُنشأة';
+
+  @override
+  String get generate_password => 'انقر على إنشاء كلمة مرور جديدة';
+
+  @override
+  String get copy => 'نسخ';
+
+  @override
+  String get generate => 'إنشاء';
+
+  @override
+  String get password_complexity => 'تعقيد كلمة المرور';
+
+  @override
+  String get poor => 'ضعيف';
+
+  @override
+  String get ok => 'جيد';
+
+  @override
+  String get fine => 'جيد';
+
+  @override
+  String get great => 'ممتاز';
+
+  @override
+  String get excellent => 'ممتاز';
+
+  @override
+  String get password_length => 'طول كلمة المرور';
+
+  @override
+  String get alphabet => 'الأبجدية';
+
+  @override
+  String get aabbcc => 'AaBbCc';
+
+  @override
+  String get numbers => 'الأرقام';
+
+  @override
+  String get numbers_string => '0123';
+
+  @override
+  String get symbols => 'الرموز';
+
+  @override
+  String get symbols_string => '!@#\$%';
+
+  @override
+  String get new_entry => 'إدخال جديد';
+
+  @override
+  String get forge_new_entry => 'إنشاء إدخال جديد';
+
+  @override
+  String get new_entry_string => '.تهيئة مجموعة جديدة من بيانات الاعتماد';
+
+  @override
+  String get title => 'اللقب';
+
+  @override
+  String get username => 'اسم المستخدم';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get required => '*مطلوب';
+
+  @override
+  String get password_generation_settings => 'إعدادات إنشاء كلمة المرور';
+
+  @override
+  String get extra_notes => 'ملاحظات إضافية';
+
+  @override
+  String get notes_go_here => 'ضع الملاحظات هنا...';
+
+  @override
+  String get secure_entry => 'إدخال آمن';
+
+  @override
+  String get entry_details => 'تفاصيل الإدخال';
+
+  @override
+  String get current_entry => 'تفاصيل الإدخال';
+
+  @override
+  String get security_health => 'حالة الأمان';
+
+  @override
+  String get date_created => 'تاريخ الإنشاء';
+
+  @override
+  String get settings => 'الإعدادات';
+
+  @override
+  String get security_protocols => 'بروتوكولات الأمان';
+
+  @override
+  String get pin_lock => 'PIN قفل';
+
+  @override
+  String get pin_lock_string =>
+      '.قم بإعداد رقم تعريف شخصي مكون من 6 أرقام كطبقة أمان إضافية';
+
+  @override
+  String get pin_unlock_string =>
+      ' تم تعيين رمز مسبقًا، انقر هنا لتغييره، اضغط مطولًا لإزالته';
+
+  @override
+  String get clipboard_clearing => 'مسح الحافظة';
+
+  @override
+  String get clipboard_clearing_string =>
+      '.يمسح الحافظة بعد 30 ثانية من نسخ بيانات الاعتماد';
+
+  @override
+  String get vault_preferences => 'إعدادات الخزنة';
+
+  @override
+  String get dark_mode => 'الوضع الداكن';
+
+  @override
+  String get dark_mode_string => 'ثيم Ironclad Obsidian';
+
+  @override
+  String get system_language => 'لغة النظام';
+
+  @override
+  String get data_operations => 'عمليات البيانات';
+
+  @override
+  String get export_credentials => 'تصدير بيانات الاعتماد';
+
+  @override
+  String get wipe_the_vault => 'مسح الخزنة';
+}
